4fe8ca95dbf3b11b1474c3e8be46d19bb6cc0bcc
arani <aranimontes@protonmail.com>
2022-12-23 12:07:30 +0100

    added swap router scrapping

--- Scraping_memepool_unilimit.js
+++ Scraping_memepool_unilimit.js
@@ -1,12 +1,13 @@
+//-------------Getting swaps fron SwaprouterV2 on Goerli ------------
 import { ethers } from "ethers";
-import abiUnilimitGoerli from "./constants/abiUnilimitGoerli.json" assert { type: "json" };
+import abiSwapRouterV2 from "./constants/abiSwapRouterV2.json" assert { type: "json" };
 import abiFlashBotTest from "./constants/abiFlashBotTest.json" assert { type: "json" };
 import contractAddress from "./constants/contractAddress.json" assert { type: "json" };
 import { config } from "dotenv";
 config();
 
 export async function listenMemepool() {
-  const newInterface = new ethers.utils.Interface(abiFlashBotTest);
+  const newInterface = new ethers.utils.Interface(abiSwapRouterV2);
   const provider = new ethers.providers.AlchemyProvider.getWebSocketProvider(
     "goerli",
     //"homestead",
@@ -14,22 +15,29 @@ export async function listenMemepool() {
   );
 
   const txFound = [];
-  const currentPool = contractAddress.UnilimitGoerli; //current pool we track
+  const currentPool = contractAddress.SwapRouterV2; //current pool we track
   const filter1 = currentPool;
-  const functionEvent = "createOrder(bool,uint160,uint256)";
-  const functionSignature = "0x623efcbe"; //createOrder signature
+  //const functionEvent = "createOrder(bool,uint160,uint256)";
+  //const functionEvent = "swap(address,address,int256,int256,uint160,uint128,int24)";
+  const functionEvent = "multicall(uint256,bytes[])";
+  //const functionSignature = "0x623efcbe"; //createOrder signature
+  //const functionSignature = "0xa5f54bcc"; // swap
+  const functionSignature = "0x5ae401dc"; //multicall
 
   provider.on("pending", async (tx) => {
     const txData = await provider.getTransaction(tx);
     console.log(txData.to);
     if (txData) {
       if (txData.to == filter1 && txData.data.includes(functionSignature)) {
+        console.log("found");
+        console.log("txdata");
+        console.log(txData);
         const gasPrice = txData.gasPrice;
         const gasLimit = txData.gasLimit;
         const from = txData.from;
         const to = txData.to;
         const value = txData.value;
-        console.log("found");
+
         let decoded = newInterface.decodeFunctionData(
           functionEvent,
           txData.data
@@ -57,11 +65,12 @@ async function logTxData(data, gasPrice, gasLimit, from, value) {
   console.log(`gasLimit: ${gasLimit}`);
   console.log(`from: ${from}`);
   console.log(`value: ${value}`);
-  console.log("--------------------------------------------------");
+  console.log(
+    "-------------------------------------------------------------------"
+  );
 }
 
 listenMemepool().catch((err) => {
   console.error(err);
   process.exitCode = 1;
-  listenMemepool();
 });
